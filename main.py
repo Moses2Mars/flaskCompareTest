2cf76d927119161b726784e740f3b5db992088ba
unknown <moussaassaf@gmail.com>
2022-04-02 13:11:34 +0100

    string cleaning

--- main.py
+++ main.py
@@ -13,20 +13,56 @@ def hello():
 @app.route('/cv-form', methods=['POST'])
 async def form_example():
     if request.method == 'POST':
-        email = request.form.get('email')
-        job_uuid = request.form.get('job_uuid')
+        #job_description is already a string, to be compared with the CV
         job_description = request.form.get('job_description')
+        
+        #CV file from the request
         CV_TEST = request.files['file']
+        
+        #change from docx format to text (string) format
         resume = docx2txt.process(CV_TEST)
+
+        #making sure job description is in string format
         text_resume = str(resume)
+        text_resume = cleanString(text_resume)
+        
+        #making sure job description is in string format
         text_description = str(job_description)
-        text_list = [text_resume, text_description] 
+        text_description = cleanString(text_description)
+
+        #making a list of both the resume and job description
+        text_list = [text_resume, text_description]
+
+        # Convert a collection of text documents to a matrix of token counts 
         cv = CountVectorizer()
         count_matrix = cv.fit_transform(text_list)
+
+        #Cosine similarity is a metric used to measure how similar the documents are irrespective of their size. 
+        # Mathematically, it measures the cosine of the angle between two vectors projected in a multi-dimensional space. 
+        # The cosine similarity is advantageous because even if the two similar documents are far apart by the Euclidean distance 
+        # (due to the size of the document), chances are they may still be oriented closer together. 
+        # The smaller the angle, the higher the cosine similarity.
         matchPercentage = cosine_similarity(count_matrix)[0][1] * 100
         matchPercentage = round(matchPercentage, 2) # round to two decimal
+
+        #the match percentage should be saved in the database along with the job_uuid, and the user email
         print('Your resume matches about '+ str(matchPercentage)+ " percent of the job description.")
-        return '''
-                    <h1>The email is: {}</h1>
-                    <h1>The cv is: {}</h1>
-                        '''.format(email, CV_TEST)
+        return str(matchPercentage)
+
+def cleanString(stringer): 
+    from nltk.tokenize import word_tokenize
+    tokens = word_tokenize(stringer)
+    # convert to lower case
+    tokens = [w.lower() for w in tokens]
+    # remove punctuation from each word
+    import string
+    table = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    stripped = [w.translate(table) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    words = [word for word in stripped if word.isalpha()]
+    # filter out stop words
+    from nltk.corpus import stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+    words = ' '.join(words)
+    return str(words)
